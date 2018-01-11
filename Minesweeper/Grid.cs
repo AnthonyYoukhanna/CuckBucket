3294700fbcf48d5d271fc0685c4c473df411b23d
AnthonyYoukhanna <anthonyyoukhanna@outlook.com>
2018-01-11 14:08:40 -0500

    First upload

---
+++ Minesweeper/Grid.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Minesweeper
+{
+   public class Grid
+    {
+       //private fields
+       private Tile[,] mGrid;
+       private int mRows, mColumns;
+       private int mTileSize;
+
+
+       //Constructors
+
+       public Grid(int Rows, int Columns, int TileSize)
+       {
+            this.mRows = Rows;
+            this.mColumns = Columns;
+            this.mTileSize = TileSize;
+
+            mGrid = new Tile[this.mRows, this.mColumns];
+
+            for (int i = 0; i < this.mRows; i++)
+            {
+                for (int j = 0; j < this.mColumns; j++)
+                {
+                    mGrid[i, j] = new Tile();
+                }
+            }
+
+            int eBombs = 10;
+            int iBombs = 40;
+            for (int i = 0; i < mGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < mGrid.GetLength(1); j++)
+                {
+                    if (mGrid.GetLength(0) == 10)
+                    {
+                        if (eBombs > 0)
+                        {
+                            mGrid[i, j].Bomb = true;
+                            eBombs--;
+                            mGrid[i, j].BackgroundColour = Color.Black;
+
+                        }
+                    }
+                    if (mGrid.GetLength(0) == 16)
+                    {
+                        if (iBombs > 0)
+                        {
+                            mGrid[i, j].Bomb = true;
+                            iBombs--;
+                            mGrid[i, j].BackgroundColour = Color.Black;
+                        }
+                    }
+                }
+            }
+
+            Color Temp;
+            Random Index = new Random();
+            for (int i = 0; i < mGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < mGrid.GetLength(1); j++)
+                {
+                    int Row = Index.Next(0, mGrid.GetLength(0) - 1);
+                        int Column = Index.Next(0, mGrid.GetLength(1) - 1);
+
+
+                        Temp = mGrid[i, j].BackgroundColour;
+                        mGrid[i, j].BackgroundColour = mGrid[Row, Column].BackgroundColour;
+                        mGrid[Row, Column].BackgroundColour = Temp;
+                }   
+            }
+            BombCount(0);
+            for (int i = 0; i < mGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < mGrid.GetLength(1); j++)
+                {
+                    if (mGrid[i,j].BombCount > 0)
+                    {
+                         
+                    }
+                }
+            }
+       }
+
+       //Methods
+
+       //Gets tile location
+       public Tile GetTile(int Row, int Columns)
+       {
+           return mGrid[Row, Columns];
+       }
+
+       public void Draw(Graphics g, int X, int Y)
+       {
+           int pX = X;
+           int pY = Y;
+
+           for (int i = 0; i < this.mRows; i++)
+           {
+
+               pY = Y + (i * this.mTileSize);
+
+               for (int j = 0; j < this.mColumns; j++)
+               {
+                   pX = X + (j * this.mTileSize);
+                   mGrid[i, j].Draw(g, pX, pY);
+
+               }
+           }
+       }
+       public int BombCount(int Bombs)
+       {
+           for (int i = 0; i < mGrid.GetLength(0); i++)
+           {
+               for (int j = 0; j < mGrid.GetLength(1); j++)
+               {
+                   if (i == 0)
+                   {
+                       if (j==0)
+                       {
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                       else if (j == mGrid.GetLength(1) - 1)
+                       {
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+
+                       }
+                       else
+                       {
+                           if (mGrid[i, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                       
+                   }
+                   else if (i == mGrid.GetLength(0) - 1)
+                   {
+                       if (j == 0)
+                       {
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                       if (j == mGrid.GetLength(1) - 1)
+                       {
+                           if (mGrid[i - 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                       else
+                       {
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                   }
+                   else
+                   {
+                       if (j == 0)
+                       {
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                       }
+                       else if (j == mGrid.GetLength(1) - 1)
+                       {
+                           if (mGrid[i - 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                          
+                       }
+                       else
+                       {
+                           if (mGrid[i - 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i - 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j - 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+                           if (mGrid[i + 1, j + 1].Bomb == true)
+                           {
+                               Bombs++;
+                           }
+ 
+                       }
+                   }
+                  
+                   
+                   mGrid[i, j].BombCount = Bombs;
+               }
+               Bombs = 0;
+           }
+           return Bombs;
+           
+       }
+
+    }
+}
---
+++ Minesweeper/Tiles.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Minesweeper
+{
+   public class Tile
+    {
+       private int mSize;
+       private bool mSelected;
+       private Color mBackgroundColour;
+       private Color mBorderColour;
+       private bool mBomb;
+       private int mBombCount;
+
+
+       public Tile()
+       {
+           this.mSize = 30;
+           this.mBackgroundColour = Color.Red;
+           this.mBorderColour = Color.White;
+       }
+
+       public Tile(int Size, Color BackgroundColour, Color BorderColour)
+       {
+           this.mSize = Size; 
+           this.mBackgroundColour = BackgroundColour;
+           this.mBorderColour = BorderColour;
+       }
+
+       //Methods
+       public void Draw(Graphics g, int X, int Y)
+       {
+           //will draw a cell on graphics object
+           //x and y represent the location of 
+           //top left corner of the cell
+
+           //create a pen and a brush to draw with
+           Pen BorderPen = new Pen(this.mBorderColour);
+           SolidBrush BackBrush = new SolidBrush(this.mBackgroundColour);
+
+           //draw cell
+           g.FillRectangle(BackBrush, X, Y + 20, this.mSize, this.mSize);
+           g.DrawRectangle(BorderPen, X, Y + 20, this.mSize, this.mSize);
+            
+
+           //dispose of drawing objects
+           BorderPen.Dispose();
+           BackBrush.Dispose();
+
+       }
+
+       public int Size
+        {
+            set { this.mSize = value; }
+            get { return this.mSize; }
+        }
+
+        public Color BackgroundColour
+        {
+            set { this.mBackgroundColour = value; }
+            get { return this.mBackgroundColour; }
+
+        }
+
+        public Color BorderColour
+        {
+            set { this.mBorderColour = value; }
+            get { return this.mBorderColour; }
+
+        }
+       
+        public bool Selected
+        {
+            set { this.mSelected = value; }
+            get { return this.mSelected; }
+
+        }
+        public bool Bomb
+        {
+            set { this.mBomb = value; }
+            get { return this.mBomb; }
+
+        }
+        public int BombCount
+        {
+            set { this.mBombCount = value; }
+            get { return this.mBombCount; }
+        }
+
+    }
+}
